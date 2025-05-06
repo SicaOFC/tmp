e476aa6caf75a2f64134d7dc7d44a14574f047cf
SicaOFC <ofc.sica@gmail.com>
2025-05-06 09:28:19 -0300

    Update Sica2.sql

--- Sica2.sql
+++ Sica2.sql
@@ -4,12 +4,17 @@ use sica;
 
 create table Aluno 
 (
-RM		int(6) primary key not null,
+id    serial primary key,
+RM		int(6) not null unique,
 Nome	varchar(30) not null,
-Curso	varchar(20),
-Email	varchar(30) not null unique,
-Senha	varchar(20) not null,
-Tell 	varchar(20) not null
+Curso	varchar(20) not null,
+Email	varchar(30) not null,
+Senha	text not null,
+Tell 	varchar(20) not null,
+dataCriacao timestamp default CURRENT_TIMESTAMP
+verificado BOOLEAN DEFAULT FALSE,
+codigoVerificacao INTEGER,
+dataCriacaoCodigo TIMESTAMP
 );
 
 ALTER TABLE Aluno
@@ -18,7 +23,7 @@ ADD CONSTRAINT min_Nome CHECK (LENGTH(Nome) >= 10);
 
 create table Prof 
 (
-RM		int(6) primary key not null,
+RM		serial primary key,
 Nome	varchar(30) not null,
 Email	varchar(30) not null unique,
 Senha	varchar(20) not null,
@@ -31,42 +36,36 @@ ADD CONSTRAINT min_Nome CHECK (LENGTH(Nome) >= 10);
 
 create table Agendamento
 (
-id int(10) Primary key auto_increment,
-RM int(6) not null,
-constraint FK_AgenProf foreign key (RM) references Prof(RM),
-data_hora datetime not null,
+id serial Primary key,
+Prof int(6) not null,
+constraint FK_AgenProf foreign key (Prof) references Prof(RM),
+data_hora Timestamp not null default current_timestamp,
 descri varchar(200) not null, -- descrição é o que vai ser feito, ex: partida de volei entre o 3DS e o 3MH
 estado varchar(100) default 'Pendente' -- estado = pendente, concluido, cancelado etc
 );
 
-
-
-create table Volei
+create table Time
 (
-id		int(6) primary key not null auto_increment,
-RM		int(6) not null,
-constraint FK_VoleiAlu foreign key (RM) references Aluno(RM)
+  id     serial primary key,
+  Nome   varchar(30) not null,
+  dataCriacao timestamp default current_timestamp
 );
 
-create table Basquete
-(
-id		int(6) primary key not null auto_increment,
-RM		int(6) not null,
-constraint FK_BasqueteAlu foreign key (RM) references Aluno(RM)
-);
+create table TimeAluno
+  (
+  cod    serial primary key,
+  Time   int(10) not null,
+  constraint FK_TimeTA foreign key (Time) references Time(id),
+  Aluno  integer not null,
+  constraint FK_AlunoTA foreign key (Aluno) references Aluno(id)
+  );
 
-create table Futebol
-(
-id		int(6) primary key not null auto_increment,
-RM		int(6) not null,
-constraint FK_FutebolAlu foreign key (RM) references Aluno(RM)
-);
 
-create table Xadrez
-(
-id		int(6) primary key not null auto_increment,
-RM		int(6) not null,
-constraint FK_XadrezAlu foreign key (RM) references Aluno(RM)
-);
+
+create table Jogo -- time, Agendamento, Esporte
+  (
+  id  serial primary key
+  
+  );
 
 SELECT * FROM Cadastro;
